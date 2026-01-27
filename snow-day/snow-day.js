645b2cfe3e6a648514110beaa280cd278cce9c9f
mrycdng <163579194+mrycdng@users.noreply.github.com>
2026-01-27 02:57:22 -0500

    snow day js update

--- snow-day/snow-day.js
+++ snow-day/snow-day.js
@@ -1,6 +1,6 @@
 document.getElementById("btn").onclick = () => {
     const link = document.createElement("a");
     link.href="snow-day.png";
-    link.download = "i’ll-keep-it-safe.png";
+    link.download = "remember-this-for-me.png";
     link.click();
 }
